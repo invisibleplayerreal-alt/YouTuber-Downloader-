05d21b2e671d60ba8c0b7529751b6a5779b7b7a0
Ziyad Kerimli <invisibleplayerreal@gmail.com>
2026-06-02 19:34:22 +0400

    Fix: Add error handling and correct filename logic

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import yt_dlp                                   #Library to download YouTube videos
+import yt_dlp                                  
 
-url = input("Enter the URL: ")                  #Get YouTube video URL from user
+url = input("Enter the URL: ")                 
 
 ydl_opts = {
-    'format': 'best',                           #Download parametr
-    'outtmpl': 'Test.%(ext)s'                   #Test = video name, (extension added automatically)
+    'format': 'best',                           
+    'outtmpl': '%(title)s.%(ext)s'              
 }
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])                         #Download the video
+    ydl.download([url])                  
 
-print("Download Completed")                     #Notify when finished.
+try:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    print("Download Completed!")
+except Exception as e:
+    print(f"Download failed. Error: {e}")
+
+                 
+
+
+
